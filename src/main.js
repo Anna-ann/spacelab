3aed5209616fcf998793d153aae4d34c178742ef
Anna-ann <sumatotstuki@gmail.com>
2023-08-18 12:20:15 +0200

    third commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-import { createApp } from 'vue/dist/vue.esm-bundler'
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap/dist/js/bootstrap.js"
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import router from './router'
-
-
-import App from './App.vue'
+import { createApp } from 'vue';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import router from './router';
+import App from './App.vue';
 
 const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 const initialTheme = document.body.getAttribute('data-theme') || (prefersDark ? 'dark' : 'light');
@@ -13,6 +11,3 @@ const initialTheme = document.body.getAttribute('data-theme') || (prefersDark ? 
 const app = createApp(App);
 app.use(router).mount('#app');
 app.config.globalProperties.isDarkTheme = initialTheme === 'dark';
-
-
-

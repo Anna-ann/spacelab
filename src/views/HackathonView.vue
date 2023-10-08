88446d5ae52be4e621f85a282b4a83250a9df7b1
Anna-ann <sumatotstuki@gmail.com>
2023-10-08 16:03:22 +0200

    hack

--- src/views/HackathonView.vue
+++ src/views/HackathonView.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <Hero />
+    <Map />
   </div>
 </template>
 
 <script>
 import Hero from '/src/components/hackathon/Hero.vue';
+import Map from '/src/components/hackathon/Map.vue';
 
 export default {
   components: {
     Hero,
+    Map,
   },
 };
 </script>

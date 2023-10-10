5020dfc1c00fad2b992e9d1d85c2af566ab7a976
Anna-ann <sumatotstuki@gmail.com>
2023-10-10 17:43:12 +0200

    16th right one

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,75 +1,62 @@
 <template>
   <header>
-  <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark">
-    <div class="container">
-      <router-link class="navbar-brand" to="/">
-        <img src="/src/assets/logo.svg" width="120" height="55">
-      </router-link>
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark">
+      <div class="container">
+        <router-link class="navbar-brand" to="/">
+          <img src="/src/assets/logo.svg" width="120" height="55">
+        </router-link>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <router-link class="nav-link" to="/">{{$t('navbar.home')}}</router-link>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#about">{{$t('navbar.about')}}</a>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/space3d">Space3D</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/spaceiot">SpaceIoT</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/spacecubs">SpaceCUBS</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/hackathon">NASA Hackathon</router-link>
-          </li>
-        </ul>
-      </div>
-
-      <div class="navbar-icons">
-        <form class="d-flex">
-          <button class="btn btn-primary" type="submit">
-            <a href="#contact">{{$t('navbar.contact')}}</a>
-          </button>
-        </form>
-        <div :class="['dropdown', { 'dropdown-dark': isDarkTheme }]">
-          <button
-            class="btn btn-link navbar-icon dropdown-toggle custom-dropdown-toggle"
-            type="button"
-            id="languageDropdown"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i class="bi bi-globe"></i>
-          </button>
-          <div class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="languageDropdown">
-            <a
-              v-for="language in supportedLanguages"
-              :key="language"
-              class="dropdown-item"
-              :class="{ active: currentLocale === language }"
-              @click="changeLanguage(language)"
-            >
-              {{ language }}
-            </a>
-          </div>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/">{{ $t('navbar.home') }}</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/space3d">Space3D</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/spaceiot">SpaceIoT</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/spacecubs">SpaceCUBS</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/hackathon">NASA Hackathon</router-link>
+            </li>
+          </ul>
         </div>
 
-        <button class="btn btn-link navbar-icon" @click="toggleTheme" title="Toggle Theme">
-          <i :class="isDarkTheme ? 'bi bi-moon-fill' : 'bi bi-brightness-high-fill'" width="90" height="90"></i>
-        </button>
+        <div class="navbar-icons">
+          <form class="d-flex">
+            <button class="btn btn-primary" type="submit">
+              <a href="#contact">{{ $t('navbar.contact') }}</a>
+            </button>
+          </form>
+          <div :class="['dropdown', { 'dropdown-dark': isDarkTheme }]">
+            <button class="btn btn-link navbar-icon dropdown-toggle custom-dropdown-toggle" type="button"
+              id="languageDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="bi bi-globe"></i>
+            </button>
+            <div class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="languageDropdown">
+              <a v-for="language in supportedLanguages" :key="language" class="dropdown-item"
+                :class="{ active: currentLocale === language }" @click="changeLanguage(language)">
+                {{ language }}
+              </a>
+            </div>
+          </div>
+
+          <button class="btn btn-link navbar-icon" @click="toggleTheme" title="Toggle Theme">
+            <i :class="isDarkTheme ? 'bi bi-moon-fill' : 'bi bi-brightness-high-fill'" width="90" height="90"></i>
+          </button>
+        </div>
       </div>
-    </div>
-  </nav>
-</header>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -77,16 +64,16 @@ export default {
   name: 'Navbar',
   data() {
     return {
-      supportedLanguages: ['en', 'kz', 'ru'], 
+      supportedLanguages: ['en', 'kz', 'ru'],
       currentLocale: 'en',
       isDarkTheme: true,
     };
   },
   methods: {
     changeLanguage(locale) {
-    this.currentLocale = locale;
-    this.$i18n.locale = locale;
-    localStorage.setItem('selectedLanguage', locale);
+      this.currentLocale = locale;
+      this.$i18n.locale = locale;
+      localStorage.setItem('selectedLanguage', locale);
     },
     toggleTheme() {
       this.isDarkTheme = !this.isDarkTheme;
@@ -113,49 +100,50 @@ export default {
     },
   },
   created() {
-  const savedTheme = localStorage.getItem('isDarkTheme');
+    const savedTheme = localStorage.getItem('isDarkTheme');
 
-  if (savedTheme === 'true') {
-    this.isDarkTheme = true;
-  } else if (savedTheme === 'false') {
-    this.isDarkTheme = false;
-  }
-
-  this.updateThemeClasses();
-
-  const selectedLanguage = localStorage.getItem('selectedLanguage');
-
-  const userPreferredLanguage = navigator.language.substr(0, 2);
-
-  if (selectedLanguage) {
-    this.currentLocale = selectedLanguage;
-    this.$i18n.locale = selectedLanguage;
-  } else {
-    if (this.supportedLanguages.includes(userPreferredLanguage)) {
-      this.currentLocale = userPreferredLanguage;
-      this.$i18n.locale = userPreferredLanguage;
+    if (savedTheme === 'true') {
+      this.isDarkTheme = true;
+    } else if (savedTheme === 'false') {
+      this.isDarkTheme = false;
     }
-  }
-},
+
+    this.updateThemeClasses();
+
+    const selectedLanguage = localStorage.getItem('selectedLanguage');
+
+    const userPreferredLanguage = navigator.language.substr(0, 2);
+
+    if (selectedLanguage) {
+      this.currentLocale = selectedLanguage;
+      this.$i18n.locale = selectedLanguage;
+    } else {
+      if (this.supportedLanguages.includes(userPreferredLanguage)) {
+        this.currentLocale = userPreferredLanguage;
+        this.$i18n.locale = userPreferredLanguage;
+      }
+    }
+  },
 
 
 };
 </script>
 
 <style>
-
 header {
   width: 100%;
   background-color: var(--bg-color);
   transition: background-color 0.3s ease;
   max-width: none;
 }
+
 .navbar {
   max-width: none;
   position: sticky;
   top: 0;
   z-index: 100;
 }
+
 .navbar-icons {
   display: flex;
   align-items: center;
@@ -191,10 +179,11 @@ header {
 }
 
 .dropdown-menu.show {
-  width: 60px; 
+  width: 60px;
 }
 
-.dropdown-item.active, .dropdown-item:active {
+.dropdown-item.active,
+.dropdown-item:active {
   color: var(--bs-dropdown-link-active-color);
   text-decoration: none;
   background-color: #ff6b00;
